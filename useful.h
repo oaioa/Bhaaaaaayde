e5991f4c3200a34bdde0d483c8dd8e4be0c5685a
oaioa <thomas.viola@insa-lyon.fr>
2017-03-25 14:26:05 +0100

    Transfert de fichiers pas en core fonctionnel

--- useful.h
+++ useful.h
@@ -21,49 +21,15 @@ int delete(char buf[], int taille){
         buf[i]='\0';
     }
     return 1;
-
-}
-
-//create a UDP socket
-int make_socket(int domain, int type, int protocol){
-    int s;
-    if ( (s=socket(domain,type, protocol)) == -1){
-        die("socket");
-    }
-    return s;
-}
-
-int set_bind(int s, struct sockaddr_in *sin,socklen_t sin_size){
-    //bind socket to port
-    if( bind(s , (struct sockaddr *) sin, sin_size ) == -1)
-    {
-        die("bind");
-    }
-    return 0;
-}
-
-int set_sockaddr_in(struct sockaddr_in *sin,int port,int address){
-    // zero out the structure
-    memset((char *) sin, 0, sizeof(sin));
-    sin->sin_family = AF_INET;
-    sin->sin_port = htons(port);
-    sin->sin_addr.s_addr = htonl(address);
 }
 
 //send the message
 int send_message(int socket,char message[BUFLEN], size_t message_len,struct sockaddr *si_other,int si_other_len){
-    if (sendto(socket, message, message_len , 0 , (struct sockaddr *) si_other,si_other_len)==-1){
+
+    if (sendto(socket, message, message_len , 0 , (struct sockaddr *) si_other,si_other_len)==-1)
+    {
         die("sendto()");
         return 1;
-    }
-    return 0;
-}
-
-//converts the address and store it in struct
-int store_adress(char * adress, struct in_addr * sin){
-    if (inet_aton(adress , sin) == 0) {
-        fprintf(stderr, "inet_aton() failed\n");
-        exit(1);
     }
     return 0;
 }
@@ -81,17 +47,45 @@ int receive_message(int socket, char buf[BUFLEN],struct sockaddr *si_other, int 
 }
 
 int string_compare(char * s1, int size){ //size = strlen(buf);
-    if (size <7){
-        return -1;
-    }else{
-        int i = 0;
-        char s2 [7] = "SYN-ACK";
-        int same_letter = 0; //on suppose que la premiere lettre est ok
-        do{
-            if(s1[i] != s2[i])
-                same_letter = -1;
-            i++;
-        }while(same_letter == 0 && i <7);
-        return same_letter;
-    }
+	if (size <7){
+		return -1;
+	}else{
+		int i = 0;
+		char s2 [7] = "SYN-ACK";
+		int same_letter = 0; //on suppose que la premiere lettre est ok
+		do{
+			if(s1[i] != s2[i])
+				same_letter = -1;
+			i++;
+		}while(same_letter == 0 && i <7);
+		return same_letter;
+	}
 }
+
+//FRAGMENTAION
+/*if ((f_in = fopen(ENTREE,"r")) == NULL)
+            {
+                fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",ENTREE);
+                return(EXIT_FAILURE);
+            }
+            if ((f_out = fopen(SORTIE,"w")) == NULL)
+            {
+                fprintf(stderr, "\nErreur: Impossible d'ecrire dans le fichier %s\n", \
+                        SORTIE);
+                return(EXIT_FAILURE);
+            }
+            while ((c = fgetc(f_in)) != EOF){
+                delete(message,1024);
+                message[counter_fragment]=c; 
+                counter_fragment++;
+                while (counter_fragment<1024 && ((c = fgetc(f_in)) != EOF) ){
+                    message[counter_fragment]=c; 
+                    counter_fragment++;
+                }
+                send_message(s,message,1026,  (struct sockaddr *)&si_other, slen);
+                printf("J'ai envoyé %s \n",message);
+                counter_fragment = 0;
+            }
+            fclose(f_in);
+            fclose(f_out);
+*/
